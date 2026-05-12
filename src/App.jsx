879e0586741c94e135be6e27ab12fa6512fbd41c
armstrong-khisa <armstrong.khisa@student.moringaschool.com>
2026-05-12 14:41:56 +0300

    fixed the navigation bar export and routings to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function  App(){
   return (
         <BrowserRouter>
                 <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/shop' element={<Shop />} />
+                    <Route path='/admin' element={<Admin />} />
+                </Routes>
         </BrowserRouter>
   )
 }
