a6bd373837426b687d0f51694859015bb93274f7
Derrick <derrick@example.com>
2026-05-13 14:13:19 +0300

    Added functionality whereby admin can search thorugh inventory from the Admin page

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,16 @@
 import { useProducts } from '../context/ProductsContext';
 import ProductCard from './ProductCard';
+import { useState } from 'react';
 
 export default function ProductList({ onEdit, className = '' }) {
   // Pull everything from Context
   const { products, loading, removeProduct } = useProducts();
+
+  const [search, setSearch] = useState('');
+  const filteredProducts = products.filter((p) =>
+    p.name.toLowerCase().includes(search.toLowerCase()) ||
+    p.origin.toLowerCase().includes(search.toLowerCase())
+  );
 
   if (loading) {
     return (
@@ -24,8 +31,17 @@ export default function ProductList({ onEdit, className = '' }) {
   }
 
   return (
+  <div>
+    <input
+    type='text'
+    placeholder="Search blends or origins..."
+    value={search}
+    onChange={(e) => setSearch(e.target.value)}
+    classsname="w-full p-4 mb-6 rounded-2x1 border border-stone-300"
+    />
+   
     <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ${className}`}>
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <ProductCard
           key={product.id}
           product={product}
@@ -34,5 +50,6 @@ export default function ProductList({ onEdit, className = '' }) {
         />
       ))}
     </div>
-  );
+  </div>
+  )
 }
