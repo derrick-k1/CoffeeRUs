89a99dddbec3e5d88e756c9551b925a8a49c77d5
armstrong-khisa <armstrong.khisa@student.moringaschool.com>
2026-05-13 13:05:58 +0300

    feat: wrap application in ProductsProvider and setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import  Navbar  from './components/Navbar'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Admin from './pages/Admin'
 import Shop from './pages/Shop'
-import ProductCard from './components/ProductCard'
 
-function  App(){
+// 1. Import the Provider you just created
+import { ProductsProvider } from './context/ProductsContext'
+
+function App() {
   return (
-        <BrowserRouter>
-                <Navbar/>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/shop' element={<Shop />} />
-                    <Route path='/admin' element={<Admin />} />
-                </Routes>
-        </BrowserRouter>
+    /* 2. Wrap the entire app so Context is available everywhere */
+    <ProductsProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/admin' element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </ProductsProvider>
   )
 }
 
